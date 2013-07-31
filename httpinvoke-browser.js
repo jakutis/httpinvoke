209ed9a61e41c1dabb156b84162f656190f927be
Vytautas Jakutis <vytautas@jakut.is>
2013-07-31 20:07:09 +0300

    initial commit

---
+++ httpinvoke-browser.js
@@ -0,0 +1,178 @@
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.httpinvoke = factory();
+  }
+}(this, function () {
+    var createXHR = function() {
+        var candidates = ['Microsoft.XMLHTTP', 'Msxml2.XMLHTTP.6.0', 'Msxml2.XMLHTTP.3.0', 'Msxml2.XMLHTTP'].map(function(type) {
+            return function() {
+                return new ActiveXObject(type);
+            };
+        });
+        candidates.unshift(function() {
+            return new XMLHttpRequest();
+        });
+
+        var xhr;
+        if(candidates.some(function(create) {
+            try {
+                xhr = create();
+                createXHR = create;
+                return true;
+            } catch(e) {
+                return false;
+            }
+        })) {
+            return xhr;
+        } else {
+            createXHR = function() {
+                throw new Error('Cannot construct XMLHttpRequest');
+            };
+            return createXHR();
+        }
+    };
+
+    var parseHeader = function(header) {
+        var colon = header.indexOf(':');
+        return {
+            name: header.slice(0, colon).toLowerCase(),
+            value: header.slice(colon + 1).trim()
+        };
+    };
+
+    var noop = function() {};
+    var readyStates = ['UNSENT', 'OPENED', 'HEADERS_RECEIVED', 'LOADING', 'DONE'];
+    return function(uri, method, options) {
+        options = options || {};
+        var uploadProgressCb = options.uploading || noop;
+        var downloadProgressCb = options.downloading || noop;
+        var statusCb = options.gotStatus || noop;
+        var cb = options.finished || noop;
+        var deleteCallbacks = function() {
+            uploadProgressCb = null;
+            downloadProgressCb = null;
+            statusCb = null;
+            cb = null;
+        };
+        var data = options.data || null;
+        var input, inputLength, inputHeaders = options.headers || [];
+        var output, outputLength, outputHeaders = {};
+        var xhr = createXHR();
+
+        xhr.upload.onloadstart = function(progressEvent) {
+        };
+        xhr.upload.onloadend = function(progressEvent) {
+        };
+        xhr.upload.ontimeout = function(progressEvent) {
+            if(cb) {
+                cb(progressEvent);
+                deleteCallbacks();
+            }
+        };
+        xhr.upload.onload = function(progressEvent) {
+        };
+        xhr.upload.onerror = function(progressEvent) {
+            if(cb) {
+                cb(progressEvent);
+                deleteCallbacks();
+            }
+        };
+        xhr.upload.onabort = function(progressEvent) {
+        };
+        xhr.upload.onprogress = function(progressEvent) {
+            if(progressEvent.lengthComputable) {
+                uploadProgressCb(0, progressEvent.loaded, inputLength);
+            }
+        };
+        xhr.onloadstart = function(progressEvent) {
+        };
+        xhr.onloadend = function(progressEvent) {
+        };
+
+        xhr.ontimeout = function(progressEvent) {
+            if(cb) {
+                cb(progressEvent);
+                deleteCallbacks();
+            }
+        };
+        xhr.onload = function(progressEvent) {
+        };
+        xhr.onerror = function(progressEvent) {
+            if(cb) {
+                cb(progressEvent);
+                deleteCallbacks();
+            }
+        };
+        xhr.onabort = function(progressEvent) {
+        };
+        xhr.onprogress = function(progressEvent) {
+            if(progressEvent.lengthComputable) {
+                downloadProgressCb(0, progressEvent.loaded, outputLength);
+            }
+        };
+        xhr.onreadystatechange = function(event) {
+            var readyState = readyStates[event.readyState];
+            if(readyState === 'HEADERS_RECEIVED') {
+                xhr.getAllResponseHeaders().split(/\r?\n/).map(function(line) {
+                    return line.trim();
+                }).filter(function(line) {
+                    return line.length !== 0;
+                }).map(parseHeader).forEach(function(header) {
+                    if(typeof outputHeaders[header.name] === 'undefined') {
+                        outputHeaders[header.name] = header.value;
+                    } else if(typeof outputHeaders[header.name] === 'string') {
+                        outputHeaders[header.name] = [outputHeaders[header.name], header.value];
+                    } else {
+                        outputHeaders[header.name].push(header.value);
+                    }
+                });
+                outputLength = Number(outputHeaders['content-length']);
+                uploadProgressCb(0, inputLength, inputLength);
+                uploadProgressCb = null;
+                downloadProgressCb(0, 0, outputLength);
+                statusCb(xhr.status, outputHeaders);
+                statusCb = null;
+            } else if(readyState === 'DONE') {
+                downloadProgressCb(0, outputLength, outputLength);
+                downloadProgressCb = null;
+                output = (typeof xhr.responseType === 'undefined' || xhr.responseType === '') ? 'text' : xhr.responseType;
+                if(output === 'text') {
+                    cb(null, xhr.responseText);
+                } else {
+                    cb(new Error('Unknown response body format ' + output));
+                }
+                deleteCallbacks();
+            }
+        };
+        xhr.open(method, uri, true);
+        inputHeaders.forEach(function(header) {
+            xhr.setRequestHeader(header.name, header.value);
+        });
+        if(data === null) {
+            input = null;
+            inputLength = 0;
+        } else if(typeof data === 'string') {
+            input = data;
+            inputLength = data.length;
+            xhr.setRequestHeader('Content-Length', inputLength);
+        } else {
+            throw new Error('Data type not supported: ' + (typeof data));
+        }
+        xhr.send(input);
+        uploadProgressCb(0, 0, inputLength);
+        return function() {
+            if(cb) {
+                try {
+                    xhr.abort();
+                } catch(err){
+                }
+                cb(new Error('aborted'));
+                deleteCallbacks();
+            }
+        };
+    };
+}));
