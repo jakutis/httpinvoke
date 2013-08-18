3c5c22268599cf406b01e7df63d755b537453077
Vytautas Jakutis <vytautas@jakut.is>
2013-08-18 21:26:43 +0300

    test: add corsCredentialsSpec

---
+++ test/corsCredentialsSpec.js
@@ -0,0 +1,16 @@
+var url = typeof window === 'undefined' ? 'http://example.org' : location.href;
+var httpinvoke = require('../httpinvoke-node');
+
+describe('"corsCredentials" feature flag', function() {
+    this.timeout(10000);
+    it('exists', function(done) {
+        if(typeof httpinvoke.corsCredentials !== 'boolean') {
+            return done(new Error('The httpinvoke.corsCredentials flag was not found'));
+        }
+        if(!httpinvoke.corsCredentials) {
+            console.log('CORS credentials sending is not supported');
+        }
+        done();
+    });
+});
+
