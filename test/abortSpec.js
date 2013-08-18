af93af75a5b7c36c266a182b80ef0eb7c9df4ed0
Vytautas Jakutis <vytautas@jakut.is>
2013-08-18 17:10:27 +0300

    test: add abortSpec

---
+++ test/abortSpec.js
@@ -0,0 +1,31 @@
+var url = typeof window === 'undefined' ? 'http://example.org' : location.href;
+var httpinvoke = require('../httpinvoke-node');
+
+describe('abort', function() {
+    this.timeout(10000);
+    it('ensures that no callbacks, except finished with Error, are called when invoked immediately', function(done) {
+        var callback = function() {
+            if(done === null) {
+                return;
+            }
+            done(new Error('A callback has been called'));
+            done = null;
+        };
+        var abort = httpinvoke(url, {
+            gotStatus: callback,
+            downloading: callback,
+            uploading: callback,
+            finished: function(err) {
+                if(done === null) {
+                    return;
+                }
+                if(typeof err === 'object' && err !== null && err instanceof Error) {
+                    done();
+                } else {
+                    done(new Error('"finished" was not called with Error'));
+                }
+            }
+        });
+        abort();
+    });
+});
