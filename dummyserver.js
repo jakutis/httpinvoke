f2a87ef8285f485fa9c274ff10a86694dd0943b3
Vytautas Jakutis <vytautas@jakut.is>
2013-08-21 01:48:34 +0300

    dummyserver.js: send Content-* headers only when body is sent or method=HEAD

--- dummyserver.js
+++ dummyserver.js
@@ -2,39 +2,42 @@ var http = require('http');
 var cfg = require('./dummyserver-config');
 
 http.createServer(function (req, res) {
-    var hello = function(body) {
+    var hello = function(status, body, head) {
         var str = 'Hello World\n';
         var headers = {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'OPTIONS, POST, HEAD, PUT, DELETE, GET',
-            'Access-Control-Allow-Headers': '',
-            'Content-Type': 'text/plain',
-            'Content-Length': String(str.length)
+            'Access-Control-Allow-Headers': ''
         };
+        if(body || head) {
+            headers['Content-Type'] = 'text/plain';
+            headers['Content-Length'] = String(str.length);
+            headers['Content-Range'] = 'bytes 0-' + (str.length - 1) + '/' + str.length;
+        }
         if(typeof req.headers.origin === 'string') {
             headers['Access-Control-Allow-Origin'] = req.headers.origin;
         }
-        res.writeHead(200, headers);
+        res.writeHead(status, headers);
         if(body) {
             res.end(str);
         } else {
             res.end();
         }
     };
+
     if(req.method === 'OPTIONS') {
-        hello(true);
+        hello(200, true, false);
     } else if(req.method === 'POST') {
-        hello(true);
+        hello(200, true, false);
     } else if(req.method === 'HEAD') {
-        hello(false);
+        hello(200, false, true);
     } else if(req.method === 'PUT') {
-        hello(true);
+        hello(200, true, false);
     } else if(req.method === 'DELETE') {
-        hello(true);
+        hello(200, true, false);
     } else if(req.method === 'GET') {
-        hello(true);
+        hello(200, true, false);
     } else {
-        res.writeHead(406);
-        res.end();
+        hello(406, false, false);
     }
 }).listen(cfg.port, cfg.host);
