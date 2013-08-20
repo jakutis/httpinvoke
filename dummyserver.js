2ff1d64aedfb1224558342ffe93ce0653c6eff80
Vytautas Jakutis <vytautas@jakut.is>
2013-08-20 21:39:06 +0300

    dummyserver.js: implement OPTIONS, HEAD, GET, DELETE, POST, PUT methods

--- dummyserver.js
+++ dummyserver.js
@@ -2,17 +2,12 @@ var http = require('http');
 var cfg = require('./dummyserver-config');
 
 http.createServer(function (req, res) {
-    if(req.method === 'OPTIONS') {
-        res.writeHead(200, {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, OPTIONS',
-            'Access-Control-Allow-Headers': '',
-            'Content-Length': '0'
-        });
-        res.end('');
-    } else if(req.method === 'GET') {
+    var hello = function(body) {
         var str = 'Hello World\n';
         var headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS, POST, HEAD, PUT, DELETE, GET',
+            'Access-Control-Allow-Headers': '',
             'Content-Type': 'text/plain',
             'Content-Length': String(str.length)
         };
@@ -20,7 +15,24 @@ http.createServer(function (req, res) {
             headers['Access-Control-Allow-Origin'] = req.headers.origin;
         }
         res.writeHead(200, headers);
-        res.end(str);
+        if(body) {
+            res.end(str);
+        } else {
+            res.end();
+        }
+    };
+    if(req.method === 'OPTIONS') {
+        hello(true);
+    } else if(req.method === 'POST') {
+        hello(true);
+    } else if(req.method === 'HEAD') {
+        hello(false);
+    } else if(req.method === 'PUT') {
+        hello(true);
+    } else if(req.method === 'DELETE') {
+        hello(true);
+    } else if(req.method === 'GET') {
+        hello(true);
     } else {
         res.writeHead(406);
         res.end();
