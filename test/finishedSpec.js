bc177ce671ef3a8ebca042269d66ba580155b502
Vytautas Jakutis <vytautas@jakut.is>
2013-09-03 22:42:01 +0300

    test: finishedSpec: remove has "output" be an ascii encoded string, when response Content-Type is "text/plain; charset=UTF-8"

--- test/finishedSpec.js
+++ test/finishedSpec.js
@@ -19,19 +19,4 @@ describe('"finished" option', function() {
             }
         });
     });
-    // TODO do this test for various binary and variously encoded text responses
-    it('has "output" be an ascii encoded string, when response Content-Type is "text/plain; charset=UTF-8"', function(done) {
-        var expectedOutput = unescape(encodeURIComponent('Sveika Žeme\n'));
-        httpinvoke(cfg.url + 'utf8', {
-            finished: function(err, output) {
-                if(err) {
-                    return done(err);
-                }
-                if(output !== expectedOutput) {
-                    return done(new Error('Received output "' + output + '" is not equal to the expected output "' + expectedOutput + '"'));
-                }
-                done();
-            }
-        });
-    });
 });
