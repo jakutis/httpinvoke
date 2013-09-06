3ee19fcb8174256376459a99f880198d22d8c444
Vytautas Jakutis <vytautas@jakut.is>
2013-09-07 00:57:54 +0300

    test: timeoutSpec: remove duplicate slash in url

--- test/timeoutSpec.js
+++ test/timeoutSpec.js
@@ -5,7 +5,7 @@ describe('"timeout" option', function() {
     this.timeout(10000);
     cfg.eachBase(function(postfix, url) {
         it('forces finish with error when normal finish would take longer than specified timeout ' + postfix, function(done) {
-            httpinvoke(url + '/tenseconds', {
+            httpinvoke(url + 'tenseconds', {
                 timeout: 3000,
                 finished: function(err) {
                     if(typeof err !== 'object' || err === null || !(err instanceof Error)) {
