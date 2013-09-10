c81e1ea24770f639e93d9d934831f2c9efccea1a
Vytautas Jakutis <vytautas@jakut.is>
2013-09-10 15:42:22 +0300

    test: add convertersSpec.js

---
+++ test/convertersSpec.js
@@ -0,0 +1,26 @@
+var cfg = require('../dummyserver-config');
+var httpinvoke = require('../httpinvoke-node');
+
+describe('"converters" option', function() {
+    this.timeout(10000);
+    it('basically works', function(done) {
+        httpinvoke(cfg.url + 'boolean', 'POST', {
+            input: true,
+            inputType: 'boolean',
+            outputType: 'boolean',
+            converters: {
+                'text boolean': Boolean,
+                'boolean text': String
+            },
+            finished: function(err, output) {
+                if(err) {
+                    return done(err);
+                }
+                if(output !== true) {
+                    return done(new Error('Expected output to be converted to a boolean value "true"'));
+                }
+                done();
+            }
+        });
+    });
+});
