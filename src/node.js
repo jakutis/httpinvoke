ea911f79033bfce329447f1466f1e82317969504
Vytautas Jakutis <jakutis@wix.com>
2015-03-04 09:37:17 +0200

    node: use protocol from the URI, instead of always assuming 'http:'
    Thanks to Jason Lai <jason@getpebble.com> (https://github.com/jlai), author of the following
    2015-03-04T03:32:37Z issue at https://github.com/jakutis/httpinvoke/issues/13
    
    The node version of httpinvoke ignores "https" in URLs and connects to plain http instead.
    
    For example,
    
    ```js
    var httpinvoke = require('httpinvoke');
    
    httpinvoke('https://www.eff.org', 'GET', function (err, body, status, headers) {
      console.log(status + ': ' + JSON.stringify(headers) + '\n\n' + body);
    });
    ```
    
    returns a redirect to the https url instead of the correct https content.
    
    The code in src/node.js needs to be modified to do something like
    
    ```js
    var httpOrHttps = uri.protocol === 'https:' ? https : http; // uri.protocol is always lowercase
    var req = httpOrHttps.request({
    ```

--- src/node.js
+++ src/node.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var https = require('https');
 var url = require('url');
 var zlib = require('zlib');
 
@@ -86,7 +87,15 @@ var httpinvoke = function(uri, method, options, cb) {
         res.on('end', pass);
     };
     uri = url.parse(uri);
-    var req = http.request({
+    var request = null;
+    if(uri.protocol === 'http:') {
+        request = http.request.bind(http);
+    } else if(uri.protocol === 'https:') {
+        request = https.request.bind(https);
+    } else {
+        return failWithoutRequest(cb, new Error('protocol ' + uri.protocol + ' is not among these supported protocols: http:, https:'));
+    }
+    var req = request({
         hostname: uri.hostname,
         port: Number(uri.port),
         path: uri.path,
