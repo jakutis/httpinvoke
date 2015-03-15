d4a224df5d43dddecd8884448fd1c5f41f68e585
Vytautas Jakutis <vytautas@jakut.is>
2015-03-15 13:05:29 +0200

    test/index.js: jshint

--- test/index.js
+++ test/index.js
@@ -1,21 +1,24 @@
-mocha.setup('bdd');
+/* global window */
+window.mocha.setup('bdd');
 function hack() {
+    'use strict';
     // This is for Opera 10.10 and Mocha combination.
     // Mocha uses Canvas2DContext.measureText, but Opera 10.10 does not have it.
     // It all ends in an uncaught exception and tests not run. Bad. Need to fix.
     if('CanvasRenderingContext2D' in window) {
-        if(!CanvasRenderingContext2D.prototype.measureText) {
-            CanvasRenderingContext2D.prototype.measureText = function() {
+        if(!window.CanvasRenderingContext2D.prototype.measureText) {
+            window.CanvasRenderingContext2D.prototype.measureText = function() {
                 return 0;
             };
-            CanvasRenderingContext2D.prototype.fillText = function() {
+            window.CanvasRenderingContext2D.prototype.fillText = function() {
             };
         }
     }
 }
 window.onload = function() {
+    'use strict';
     hack();
-    document.getElementById('agent').appendChild(document.createTextNode(navigator.userAgent));
-    mocha.checkLeaks();
-    mocha.run();
+    window.document.getElementById('agent').appendChild(window.document.createTextNode(window.navigator.userAgent));
+    window.mocha.checkLeaks();
+    window.mocha.run();
 };
